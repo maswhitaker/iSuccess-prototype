c33770092c4a154293f9ed94f45629e3f7635c49
maswhitaker <masforwhi@yahoo.com>
2014-08-04 09:22:53 -0400

    backbone working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,31 @@
 Parse.initialize("CGiH8mU7FWqmYhm2HXL1KZ2yusLAYc6uLGLxWKOE", "pFMs3sr9uuLC8ITdi6mK2unnU4xVARJ97grozseD");
 
+var Task = Parse.Object.extend("Task", {
+  defaults: {
+    name: '',
+    estimatedTime: 0,
+    description: ''
+  }
+});
 
-$(function() {
+var Tasks = Parse.Collection.extend("Tasks", {
+  model: Task
+});
+
+var Goal = Parse.Object.extend("Goal");
+
+var Goals = Parse.Collection.extend("Goals", {
+  model: Goal
+});
+
+
   var HomepageView = Parse.View.extend({
     el: ".container",
     events: {
       "click .log-out": "logOut",
-      "click .static-list": "toGoal"
+      "click #add-task": 'addTask',
+      "click #extra-task-button": "newTask",
+      "click #show-add": "showAdd"
     },
 
     initialize: function() {
@@ -18,21 +37,56 @@ $(function() {
       new LogInView();
     },
 
-    toGoal: function(){
-      console.log('hello');
-      new GoalView();
+    addTask: function(e){
+      var goal = new Goal();
+      goal.set("name", $("#goal-name").val());
+      goal.set("description", $("#goal-description").val());
+      goal.set("user", Parse.User.current());
+      goal.save().done(function(){
+        console.log("new task")
+        var task = new Task();
+        task.set("name", $("#name").val());
+        task.set("estimatedTime", $("#estimatedTime").val());
+        task.set("description", $("#description").val());
+        task.set("user", Parse.User.current());
+        task.set("parent", goal);
+        task.save().done(function(){
+          var extraName = $('#extra-name').val();
+          var extraTime = $("#extra-estimatedTime").val();
+          var extraDescription = $("#extra-description").val();
+          if(extraName != ''){
+            var task = new Task();
+            task.set("name", extraName);
+            task.set("estimatedTime", extraTime);
+            task.set("description", extraDescription);
+            task.set("user", Parse.User.current());
+            task.set("parent", goal);
+            task.save().done(function(){
+              console.log('extra task saved');
+            })
+          }
+        })
+      });
+    },
+    newTask: function(){
+      $("#extra-task").show();
+    },
+
+    showAdd: function(){
+      $("#adding").show();
     }
   });
 
 
 
   var GoalView = Parse.View.extend({
-    el: ".container",
-    initalize: function(){
+    template: _.template($("#goal-template").html()),
+    initialize: function(){
+      $(".container").html(this.el);
       this.render();
     },
     render: function(){
-      this.$el.html(_.template($("#goal-template").html()));
+      this.$el.html(this.template(this.model.attributes))
     }
   });
 
@@ -92,31 +146,38 @@ var LogInView = Parse.View.extend({
     }
   });
 
-  var AppView = Parse.View.extend({
 
-    initialize: function() {
-      this.render();
+
+  var AppRouter = Parse.Router.extend({
+    initialize: function(){
+    //  this.fetch = collection.fetch();
     },
-
-    render: function() {
-      // home route
+    routes: {
+      "":"homePage",
+      "goallist": "goalList",
+      "goallist/:id": "goal",
+      "goallist/:id/input": ""
+    },
+    homePage: function(){
       if (Parse.User.current()) {
         new HomepageView();
       } else {
         new LogInView();
       }
-    }
-  });
-
-  var AppRouter = Parse.Router.extend({
-    routes: {
-      "home":"homePage",
-
     },
-    homePage: function(){
-      
+    goalList: function(){
+      if (Parse.User.current()){
+        this.fetch.done(function(){
+          $('.container').html('')
+            collection.each(function(item){
+              new GoalView({model: item})
+            });
+          });
+      } else {
+        new LogInView();
+      }
     }
   });
+  var approuter = new AppRouter();
 
-  new AppView;
-});
+Parse.history.start();
